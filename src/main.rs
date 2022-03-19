1bb683b701043d92a752fe07ff1bf89a17830621
Ryan Jeffrey <ryan@ryanmj.xyz>
2022-03-19 13:52:39 -0700

    All interpreter data.

--- src/main.rs
+++ src/main.rs
@@ -2,6 +2,31 @@ extern crate sdl2;
 
 use std::env;
 
+// Includes all data needed by the interpreter.
+struct InterpreterData {
+    // V registers. 16 of them, general purpose, 8 bits.
+    v: [u8; 16],
+    // I register, 16 bits.
+    i: u16,
+    // Program counter, 16 bits.
+    pc: u16,
+    // Stack pointer, 16 bits.
+    sp: u8,
+    // The call stack, 16 levels of 16 bits each.
+    stack: [u16; 16],
+    // Delay timer @ 60Hz, 8 bits.
+    delay_timer: u8,
+    // Sound timer @ 60Hz, 8 bits.
+    sound_timer: u8,
+}
+
+union Memory {
+    // Entire memory of the emulator.
+    mem: [u8; 4096],
+    data: InterpreterData,
+}
+
+
 fn get_bin_file() -> String {
     const DEFAULT_FILE: &str = "game.bin";
     let args = env::args().collect::<Vec<String>>();
