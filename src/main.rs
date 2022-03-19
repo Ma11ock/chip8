f11e541da572b264375ed6afc2794c32968f0478
Ryan Jeffrey <ryan@ryanmj.xyz>
2022-03-19 15:10:28 -0700

    Read file as 16 bit byte pairs.

--- src/main.rs
+++ src/main.rs
@@ -36,28 +36,31 @@ union Memory {
     data: InterpreterData,
 }
 
-fn emulate(file: &[u8]) -> Result<(), String> {
+fn emulate(file: &Vec<u16>) -> Result<(), String> {
     let mut emu_state = Memory{ mem: [0u8; 4096] };
     // Read the file.
-    for i in 0..file.len() - 1 {
-        match file[i] {
+    for i in 0..file.len() {
+        // Check first nibble
+        match file[i] >> 12 {
             0 => {
                 if file[i + 1] == 0xE0 {
-                    // TODO clear display
+                    // Clear the display.
+                    // TODO 
                 } else if file[i + 1] == 0xEE {
+                    // Set PC to to stack[sp], decrement sp.
                     unsafe { emu_state.data.pop_stack() };
                 }
             },
+            // Set PC to bottom three nibbles.
             1 => {
-                
+
             },
             2 => {
                 
             },
-            _ => return Err(format!("Invalid instruction at position {i}: 0x{:x}{:x}.", file[i], file[i+1])),
+            _ => return Err(format!("Invalid instruction at position {i}: 0x{:x}.", file[i])),
         }
     }
-
     Ok(())
 }
 
@@ -74,13 +77,37 @@ fn get_bin_file() -> String {
     }
 }
 
+fn convert_bin_format(bytes: &[u8]) -> Result<Vec<u16>, String> {
+    // Because chip8 instructions are 16 bits its length (in bytes) should be even.
+    if bytes.len() % 2 == 1 {
+        return Err(String::from("Invalid input: the file is not an even length."));
+    }
+
+    let mut result: Vec<u16> = Vec::with_capacity(bytes.len() / 2);
+
+    for i in (0..bytes.len()).step_by(2) {
+        if cfg!(target_endian = "big") {
+            result.push(bytes[i] as u16 | (bytes[i + 1] as u16) << 8); 
+        } else {
+            result.push((bytes[i] as u16) << 8 | bytes[i + 1] as u16); 
+        }
+    }
+
+    Ok(result)
+}
+
 fn main() -> Result<(), String> {
     let game_file: String = get_bin_file();
     println!("Opening binary file {}.", game_file);
 
-    let program = match fs::read(game_file) {
+    let raw_program = match fs::read(game_file) {
         Ok(p) => p,
         Err(e) => return Err(e.to_string()),
+    };
+
+    let program = match convert_bin_format(&raw_program) {
+        Ok(p) => p,
+        Err(e) => return Err(e),
     };
 
     match emulate(&program) {
