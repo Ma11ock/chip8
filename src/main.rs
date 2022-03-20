a79ad4e554f1294afd62adf2c243691be286bd61
Ryan Jeffrey <ryan@ryanmj.xyz>
2022-03-20 14:51:19 -0700

    Fix compilation errors and warnings

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,5 @@
 extern crate sdl2;
+extern crate rand;
 
 use std::env;
 use std::fs;
@@ -23,8 +24,6 @@ struct InterpreterData {
     sound_timer: u8,
     // Memory.
     mem: [u8; 4096],
-    // RNG.
-    rng: ThreadRng,
 }
 
 impl InterpreterData {
@@ -37,7 +36,6 @@ impl InterpreterData {
                delay_timer: 0,
                sound_timer: 0,
                mem: [0; 4096],
-               rng: rand::thread_rng(),
         }
     }
 
@@ -62,6 +60,61 @@ impl InterpreterData {
     fn increment_pc(&self, amount: u16) -> u16 {
         self.pc + amount
     }
+}
+
+enum Instruction {
+    Cls,
+    Ret,
+    Jp(u16),
+    Call(u16),
+    // Skip next instruction if register equals value.
+    Se(u8, u8),
+    Sne(u8, u8),
+    // Skip next instruction if registers are equal.
+    SeR(u8, u8),
+    // Load byte into register.
+    Ld(u8, u8),
+    Add(u8, u8),
+    // Load register into other register.
+    LdR(u8, u8),
+    Or(u8, u8),
+    And(u8, u8),
+    Xor(u8, u8),
+    // Add first register to second register, store in first register.
+    AddR(u8, u8),
+    Sub(u8, u8),
+    Shr(u8, u8),
+    SubN(u8, u8),
+    Shl(u8, u8),
+    SneR(u8, u8),
+    LdI(u16),
+    // Jump to value + I.
+    JpI(u16),
+    Rnd(u8, u8),
+    Drw(u8, u8, u8),
+    Skp(u8, u8),
+    SkpN(u8, u8),
+    // Load delay time value.
+    LdD(u8),
+    // Load keypress, halt until key is pressed.
+    LdW(u8),
+    // Set delay time value.
+    LdSD(u8),
+    // Set sound time value.
+    LdS(u8),
+    AddI(u8),
+    // Load sprite location from V[x].
+    LdSp(u8),
+    // Store BCD repr of V[x] in I, I + 1, I + 2.
+    LdBCD(u8),
+    // Store registers V[0] to V[x] in memory starting at I.
+    LdIR(u8),
+    // Store memory at I into V[0] to V[x].
+    LdIRM(u8),
+}
+
+enum InstructionError {
+    InvalidInstruction
 }
 
 fn get_last_3_nibbles(n: u16) -> u16 {
@@ -90,6 +143,8 @@ fn invalid_instruction_message(index: usize, what: u16) -> String {
 
 fn emulate(file: &Vec<u16>) -> Result<(), String> {
     let mut emu_state = InterpreterData::new();
+
+    let mut rng = rand::thread_rng();
     
     // Read the file.
     for i in 0..file.len() {
@@ -228,7 +283,8 @@ fn emulate(file: &Vec<u16>) -> Result<(), String> {
             9 => {
                 if emu_state.get_register(get_third_nibble(cur_instruction)) !=
                     emu_state.get_register(get_second_nibble(cur_instruction)) {
-                        emu_state.increment_pc(2)
+                        //emu_state.increment_pc(2)
+                        // TODO fix
                     }
                 emu_state.increment_pc(1)
             },
@@ -237,11 +293,11 @@ fn emulate(file: &Vec<u16>) -> Result<(), String> {
                 emu_state.increment_pc(1)
             },
             0xb => {
-                get_last_3_nibbles(cur_instruction) + emu_state.get_register(0)
+                get_last_3_nibbles(cur_instruction) + emu_state.get_register(0) as u16
             },
             0xc => {
-                let rng: u8 = emu_state.rng.gen();
-                emu_state.v[get_third_nibble() as usize] = rng & get_last_2_nibbles();
+                emu_state.v[get_third_nibble(cur_instruction) as usize] =
+                    rng.gen::<u8>() & get_last_2_nibbles(cur_instruction);
                 emu_state.increment_pc(1)
             },
             0xd => {
@@ -259,9 +315,6 @@ fn emulate(file: &Vec<u16>) -> Result<(), String> {
                             emu_state.delay_timer;
                         emu_state.increment_pc(1)
                     },
-                    0x0a => {
-                        
-                    },
                     _ => return Err(invalid_instruction_message(i, cur_instruction)),
                 }
             },
@@ -271,6 +324,164 @@ fn emulate(file: &Vec<u16>) -> Result<(), String> {
     }
     Ok(())
 }
+
+// TODO replace error message with other type, not int.
+// Maybe return an invalid argument.
+fn program_to_enum(instruction: u16) -> Result<Instruction, InstructionError> {
+    type I = Instruction;
+    Ok(match instruction >> 12 {
+        0 => {
+            match get_last_2_nibbles(instruction) {
+                0xE0 => I::Cls,
+                0xEE => I::Ret,
+                _ => return Err(InstructionError::InvalidInstruction),
+            }
+        },
+        // Set PC to bottom three nibbles.
+        1 => {
+            I::Jp(get_last_3_nibbles(instruction))
+        },
+        // Function call at bottom three nibbles.
+        2 => {
+            I::Call(get_last_3_nibbles(instruction))
+        },
+        // Skip next instruction if the bottom byte is equal to the value
+        // in V[first nibble].
+        3 => {
+            I::Se(get_third_nibble(instruction), get_last_2_nibbles(instruction))
+        },
+        // Skip next instruction if V[third nibble] == bottom byte.
+        4 => {
+            I::Sne(get_third_nibble(instruction), get_last_2_nibbles(instruction))
+        },
+        // If V[third nibble] == V[second nibble] then skip next instruction.
+        5 => {
+            if get_first_nibble(instruction) == 0 {
+                I::SeR(get_third_nibble(instruction), get_second_nibble(instruction))
+            } else {
+                return Err(InstructionError::InvalidInstruction);
+            }
+        },
+        // Put the bottom byte into register V[third nibble].
+        6 => {
+            I::Ld(get_third_nibble(instruction), get_last_2_nibbles(instruction))
+        },
+        // Adds the bottom byte to the value of V[third nibble], then
+        // stores it there.
+        7 => {
+            I::Add(get_third_nibble(instruction), get_last_2_nibbles(instruction))
+        },
+        8 => {
+            match get_first_nibble(instruction) {
+                // Bitwise OR V[third nibble] and V[second nibble], store
+                // result in V[third nibble].
+                0 => {
+                    I::LdR(get_third_nibble(instruction), get_second_nibble(instruction))
+                },
+                1 => {
+                    I::Or(get_third_nibble(instruction), get_second_nibble(instruction))
+                },
+                2 => {
+                    I::And(get_third_nibble(instruction), get_second_nibble(instruction))
+                },
+                3 => {
+                    I::Xor(get_third_nibble(instruction), get_second_nibble(instruction))
+                },
+                4 => {
+                    I::Add(get_third_nibble(instruction), get_second_nibble(instruction))
+                },
+                5 => {
+                    I::Sub(get_third_nibble(instruction), get_second_nibble(instruction))
+                },
+                6 => {
+                    I::Shr(get_third_nibble(instruction), get_second_nibble(instruction))
+                },
+                7 => {
+                    I::SubN(get_third_nibble(instruction), get_second_nibble(instruction))
+                },
+                0xe => {
+                    I::Shl(get_third_nibble(instruction), get_second_nibble(instruction))
+                },
+                _ => return Err(InstructionError::InvalidInstruction),
+            }
+        },
+        9 => {
+            if get_first_nibble(instruction) == 0 {
+                I::SneR(get_third_nibble(instruction), get_second_nibble(instruction))
+            } else {
+                return Err(InstructionError::InvalidInstruction);
+            }
+        },
+        0xa => {
+            I::LdI(get_last_3_nibbles(instruction))
+        },
+        0xb => {
+            I::JpI(get_last_3_nibbles(instruction))
+        },
+        0xc => {
+            I::Rnd(get_third_nibble(instruction), get_last_2_nibbles(instruction))
+        },
+        0xd => {
+            I::Drw(get_third_nibble(instruction),
+                get_second_nibble(instruction),
+                get_first_nibble(instruction))
+        },
+        0xe => {
+            match get_first_nibble(instruction) {
+                0xe => I::Skp(get_third_nibble(instruction), get_second_nibble(instruction)),
+                0x1 => I::SkpN(get_third_nibble(instruction), get_second_nibble(instruction)),
+                _ => return Err(InstructionError::InvalidInstruction),
+            }
+        },
+        0xf => {
+            let third_nibble = get_third_nibble(instruction);
+            match get_last_2_nibbles(instruction) {
+                0x07 => {
+                    I::LdD(third_nibble)
+                },
+                0x0a => {
+                    I::LdW(third_nibble)
+                },
+                0x15 => {
+                    I::LdSD(third_nibble)
+                },
+                0x18 => {
+                    I::LdS(third_nibble)
+                },
+                0x1E => {
+                    I::AddI(third_nibble)
+                },
+                0x29 => {
+                    I::LdSp(third_nibble)
+                },
+                0x33 => {
+                    I::LdBCD(third_nibble)
+                },
+                0x55 => {
+                    I::LdIR(third_nibble)
+                },
+                0x65 => {
+                    I::LdIRM(third_nibble)
+                },
+                _ => return Err(InstructionError::InvalidInstruction),
+            }
+        },
+        // Load bottom byte into V[third nibble].
+        _ => return Err(InstructionError::InvalidInstruction),
+    })
+}
+
+fn convert_program(data: &Vec<u16>) -> Result<Vec<Instruction>, String> {
+    let mut result: Vec<Instruction> = Vec::with_capacity(data.len());
+    for (pos, i) in data.iter().enumerate() {
+        match program_to_enum(*i) {
+            Ok(d) => result.push(d),
+            Err(_) => return Err(invalid_instruction_message(pos, *i)),
+        }
+    }
+    Ok(result)
+}
+
 
 fn get_bin_file() -> String {
     const DEFAULT_FILE: &str = "game.bin";
